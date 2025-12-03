bf7008c52778be2a834ebf76cd4132c07fc3f979
mohit.jain <mohit.jain@medimaze.in>
2025-12-03 11:27:16 +0530

    Initial Commit Improved the terraform format

--- environments/production/backend.tf
+++ environments/production/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "<REPLACE_WITH_STATE_BUCKET>"
-    key    = "travel365/production/terraform.tfstate"
-    region = "eu-west-2"
+    bucket         = "<REPLACE_WITH_STATE_BUCKET>"
+    key            = "travel365/production/terraform.tfstate"
+    region         = "eu-west-2"
     dynamodb_table = "<REPLACE_WITH_DYNAMODB_TABLE>"
-    encrypt = true
+    encrypt        = true
   }
 }
