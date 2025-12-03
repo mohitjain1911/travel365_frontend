5f97ffd9f46d46c647d5e0441f63acef92e62af2
mohit.jain <mohit.jain@medimaze.in>
2025-12-03 15:53:09 +0530

    Improved chnages

--- environments/development/backend.tf
+++ environments/development/backend.tf
@@ -1,11 +1,11 @@
 terraform {
-	backend "s3" {
-		# Replace these placeholders with your real backend resources
-		bucket         = "tfstate-bucket-travel365"
-		key            = "travel365/development/terraform.tfstate"
-		region         = "eu-west-2"
-		dynamodb_table = "tfstate-bucket-travel365"
-		encrypt        = true
-	}
+  backend "s3" {
+    # Replace these placeholders with your real backend resources
+    bucket         = "tfstate-bucket-travel365"
+    key            = "travel365/development/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "tfstate-lock-table"
+    encrypt        = true
+  }
 }
 
