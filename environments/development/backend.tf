abb964b32d57ac66a0f0f28f6a034415b4824c43
mohit.jain <mohit.jain@medimaze.in>
2025-12-03 11:22:26 +0530

    Format Terraform files

--- environments/development/backend.tf
+++ environments/development/backend.tf
@@ -1,9 +1,9 @@
 terraform {
-#  backend "s3" {
-#    bucket = "<REPLACE_WITH_STATE_BUCKET>"
-#    key    = "travel365/development/terraform.tfstate"
-#   region = "eu-west-2"
-#   dynamodb_table = "<REPLACE_WITH_DYNAMODB_TABLE>"
-#   encrypt = true
-#  }
+  #  backend "s3" {
+  #    bucket = "<REPLACE_WITH_STATE_BUCKET>"
+  #    key    = "travel365/development/terraform.tfstate"
+  #   region = "eu-west-2"
+  #   dynamodb_table = "<REPLACE_WITH_DYNAMODB_TABLE>"
+  #   encrypt = true
+  #  }
 }
